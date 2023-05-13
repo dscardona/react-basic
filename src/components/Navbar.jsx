6a15aa02ed64a36e5cbb3c2228b30a7678e88f76
Diana Cardona <dscardona@gmail.com>
2023-05-13 11:03:24 -0400

    Add styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
+import logo from "../../images/Owl1.png"
+
 function Navbar() {
     return (
-        <h1>This is the Navbar component</h1>
+        <nav>
+            <img className="nav--logo" src={logo}/> 
+            <h3 className="nav--logo_text">dscardona</h3>
+            <h4 className="nav--title">Full-Stack Dev</h4>
+        </nav>
     )
 }
 
