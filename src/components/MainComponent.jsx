35e1aa6191b094e326c48445198fb89417414518
Diana Cardona <dscardona@gmail.com>
2023-05-13 11:36:33 -0400

    Add styling and content to main

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,6 +1,26 @@
 function MainComponent() {
     return (
-        <h1>This is the main component</h1>
+        <main>
+            <h1 className="main--subtitle">Front End Technologies</h1>
+                <ul className="main--list_items">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>Bootstrap</li>
+                    <li>React</li>
+                </ul>
+
+            <h1 className="main--subtitle">Back End Technologies</h1>
+                <ul className="main--list_items">
+                    <li>Node.js</li>
+                    <li>Python</li>
+                    <li>Express</li>
+                    <li>Flask</li>
+                    <li>React</li>
+                    <li>MongoDB</li>
+                    <li>SQL</li>
+                </ul>
+        </main>
     )
 }
 
